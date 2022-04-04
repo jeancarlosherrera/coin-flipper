8ee49cf32c4d6080a500ea18d952a4f0ce23b9ae
Jean C. Herrera <jeancarlosherreram@gmail.com>
2022-04-04 12:22:13 -0400

    coin container and functionality

--- src/Coin.js
+++ src/Coin.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Coin(props) {
   return (
     <div className='Coin'>
-      <img src={props.src} />
+      <img src={props.info.imgSrc} alt={props.info.side} />
     </div>
   );
 }
--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -20,11 +20,35 @@ class CoinContainer extends Component {
       numHeads: 0,
       numTails: 0,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  flipCoin() {
+    let choice = () => {
+      const randIdx = Math.floor(Math.random() * this.props.coins.length);
+      return this.props.coins[randIdx];
+    };
+    let newCoin = choice();
+    this.setState((st) => {
+      return {
+        currCoin: newCoin,
+        numFlips: st.numFlips + 1,
+        numHeads: st.numHeads + (newCoin.side === 'heads' ? 1 : 0),
+        numTails: st.numTails + (newCoin.side === 'tails' ? 1 : 0),
+      };
+    });
+  }
+  handleClick() {
+    this.flipCoin();
   }
   render() {
     return (
       <div>
-        <Coin src={this.props.coins[0].imgSrc} />
+        <h1>Let's Flip A Coin</h1>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <h4>
+          Out of {this.state.numFlips} flips, there have been{' '}
+          {this.state.numHeads} heads and {this.state.numTails} tails.
+        </h4>
         <button onClick={this.handleClick}>Flip Me!</button>
       </div>
     );
